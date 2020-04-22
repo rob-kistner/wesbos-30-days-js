4e821d30beb1ddb153e923864068049017e812cc
rob-kistner <robkistner@mac.com>
2020-04-21 19:21:55 -0500

    completed 15 (out of order)

---
+++ 15-local-storage/local-storage.js
@@ -0,0 +1,45 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+// read in the localStorage items list or
+// a blank array if there are none
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+function addItem(e) {
+  e.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text, // es6 shorthand for text: text
+    done: false
+  };
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+  this.reset();
+}
+
+function toggleDone(e) {
+  // skip unless it's an input
+  if (!e.target.matches('input')) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+      <li>
+        <input type="checkbox" data-index="${i}" id="item${i}" ${plate.done ? 'checked' : ''} />
+        <label for="item${i}">${plate.text}</label>
+      </li>
+    `;
+  }).join(''); // join array into one string
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+// populate from localStorage
+populateList(items, itemsList);
